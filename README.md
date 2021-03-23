49176e973bd9107a868193c26e89e110fde8f739
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-03-23 02:29:51 -0700

    Added basic sctructure, defined some variables, set question array. Began building question function

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Coding-Quiz
+
+
+## Description
+- What was your motivation?
+- Why did you build this project?
+- What problem does it solve?
+- What did you learn?
+
+
+## Usage
+Provide instructions and examples for use. Include screenshots as needed.
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+
---
+++ assets/script.js
@@ -0,0 +1,50 @@
+// Variables to target elements in HTML
+var quizSpace = document.querySelector(".quizSpace");
+var quizStartBtn = document.querySelector("#startBtn");
+// var quizAnswer = 
+// var quizOptions = [];
+// var timerValue = 
+// var quizScore = 
+// var highScores = 
+var quizQuestions = [
+    {
+        Question: "Which kind of file do you typically use to style HTML?",
+        A: ".js",
+        B: ".css",
+        C: ".jquery",
+        answer: "B",
+      },
+    {
+        Question: "What is HTML short for?",
+        A: "Hypertext Mark-up Language",
+        B: "Hypertext Made-up Language",
+        C: "Hypertext Model Lingo",
+        answer: "A",
+      },
+    {
+        Question: "What development language is powered entirely by coffee?",
+        A: "CSS",
+        B: "python",
+        C: "javascript",
+        answer: "C",
+    }        
+]
+
+
+// function start quiz? {}
+
+quizStartBtn.addEventListener("click", function() {
+    // window.alert('Hello?');
+    
+});
+
+
+
+// function right/wrong answer? {}
+// + function to hidelast/shownext questions/answers
+// + show "correct/wrong" indicator?
+
+// function interval for timer? {}
+// + include function for making timer = score
+
+//
